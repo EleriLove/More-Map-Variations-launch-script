2f2a033144ce6297b0fca8b44df6ba2ad9b5c23a
Eleri <141176534+EleriLove@users.noreply.github.com>
2026-06-23 18:20:41 -0400

    Update launch_nightreign.sh

--- launch_nightreign.sh
+++ launch_nightreign.sh
@@ -1,7 +1,7 @@
 cd "/home/deck/Documents/More Map Variations 2.1.6 & Weapons Mod/mod" || exit 1
 /home/deck/.local/bin/me3 launch \
-  --native "/home/deck/Documents/dum/mod/Server Redirector/cl_server_redirector.dll" \
-  --package "/home/deck/Documents/dum/mod" \
+  --native "/home/deck/Documents/More Map Variations 2.1.6 & Weapons Mod/mod/Server Redirector/cl_server_redirector.dll" \
+  --package "/home/deck/Documents/More Map Variations 2.1.6 & Weapons Mod/mod" \
   -g nightreign \
   --savefile "NR0000.co2" \
   --online true
