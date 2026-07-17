cc08a20b516ec85f008238b3ac7910e7b9f5f290
Eleri <141176534+EleriLove@users.noreply.github.com>
2026-07-16 23:41:56 -0400

    Update launch_nightreign.sh
    made non system specific

--- launch_nightreign.sh
+++ launch_nightreign.sh
@@ -1,7 +1,7 @@
-cd "/home/deck/Documents/More Map Variations 2.1.6 & Weapons Mod/mod" || exit 1
-/home/deck/.local/bin/me3 launch \
-  --native "/home/deck/Documents/More Map Variations 2.1.6 & Weapons Mod/mod/Server Redirector/cl_server_redirector.dll" \
-  --package "/home/deck/Documents/More Map Variations 2.1.6 & Weapons Mod/mod" \
+mod_engine=$(which me3)
+$mod_engine launch \
+  --native "${home}/MMV-Launcher/mod/Server Redirector/cl_server_redirector.dll" \
+  --package "${home}/MMV-Launcher/mod" \
   -g nightreign \
   --savefile "NR0000.co2" \
   --online true
