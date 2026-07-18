56998228f7b8cee37afcf7cb844d432703c3877c
Eleri <141176534+EleriLove@users.noreply.github.com>
2026-07-18 17:58:33 -0400

    Update launch_nightreign.sh

--- launch_nightreign.sh
+++ launch_nightreign.sh
@@ -3,7 +3,7 @@ prog_path=$(/usr/bin/find ~ -name "launch_nightreign.sh" 2>/dev/null)
 prog_dir=$(/usr/bin/dirname "$prog_path")
 mod_engine=$(/usr/bin/which me3)
 $mod_engine launch \
-  --native "${server_redirector" \
+  --native "${server_redirector"} \
   --package "${prog_dir}mod" \
   -g nightreign \
   --savefile "NR0000.co2" \
