e1e4c8829697868cf566391fb021c18ffdb7a1d2
Eleri <141176534+EleriLove@users.noreply.github.com>
2026-07-18 01:11:08 -0400

    Delete launch_nightreign.sh

--- launch_nightreign.sh
+++
@@ -1,7 +0,0 @@
-mod_engine=$(/usr/bin/which me3)
-$mod_engine launch \
-  --native "${home}/MMV-Launcher/mod/Server Redirector/cl_server_redirector.dll" \
-  --package "${home}/MMV-Launcher/mod" \
-  -g nightreign \
-  --savefile "NR0000.co2" \
-  --online true
