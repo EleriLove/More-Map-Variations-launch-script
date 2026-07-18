c47dbb66f005db573735835b76e64defdc9efb0a
Eleri <141176534+EleriLove@users.noreply.github.com>
2026-07-18 14:01:01 -0400

    Update general_mmv_installer.sh

--- general_mmv_installer.sh
+++ general_mmv_installer.sh
@@ -9,4 +9,5 @@ git clone https://github.com/EleriLove/More-Map-Variations-launch-script/
 cp "${HOME}/MMV-Launcher/More-Map-Variations-launch-script/launch_nightreign.sh" "${HOME}/MMV-Launcher/"
 rm -rf "${HOME}/MMV-Launcher/More-Map-Variations-launch-script"
 chmod +x "${HOME}/MMV-Launcher/launch_nightreign.sh"
-echo "launcer installed"
+echo "launcer installed, enjoy your game"
+sleep 20
