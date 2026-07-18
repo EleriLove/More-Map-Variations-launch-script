97e4798b8ae5ff56c65bdeee126c19e337b33200
SkriptKitty <elerilove@gmail.com>
2026-07-18 17:03:53 -0400

    this actually works now. just make sure to have the git repository somewhere in your home directory,

--- general_mmv_installer.sh
+++ general_mmv_installer.sh
@@ -1,19 +1,16 @@
 if grep -q '^ID=steamos$' /etc/os-release; then
     steamos-readonly disable
 fi
+prog_dir=$(find ~ -name "launch_nightreign.sh" 2>/dev/null)
 echo "downloading and installing me3..."
 curl --proto '=https' --tlsv1.2 -sSfL https://github.com/garyttierney/me3/releases/download/v0.11.0/installer.sh | sh
 echo "install done."
-cd "${1}" | exit 1
-git clone https://github.com/Daybreak-Team/MMV-Launcher
+cd "${1}" |exit 1
+git clone https://github.com/Daybreak-Team/MMV-Launcher "${1}"
 echo "mod installed"
-cd "${1}/MMV-Launcher/" | exit 1
-git clone https://github.com/EleriLove/More-Map-Variations-launch-script/
-cp "${1}/MMV-Launcher/More-Map-Variations-launch-script/launch_nightreign.sh" "${1}/MMV-Launcher/"
-rm -rf "${1}/MMV-Launcher/More-Map-Variations-launch-script"
-chmod +x "${1}/MMV-Launcher/launch_nightreign.sh"
+cp "${prog_dir}" "${1}"
 echo "launcer installed, enjoy your game"
-sleep 20
+sleep 5
 if grep -q '^ID=steamos$' /etc/os-release; then
     steamos-readonly enable
 fi
