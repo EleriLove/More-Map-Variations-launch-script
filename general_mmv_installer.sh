daaf9419a789f7c6a2113017d20f33365919696e
Eleri <141176534+EleriLove@users.noreply.github.com>
2026-07-18 14:33:47 -0400

    Update general_mmv_installer.sh
    added detection for steamos script specifics and allowed a user defined install location

--- general_mmv_installer.sh
+++ general_mmv_installer.sh
@@ -1,13 +1,19 @@
+if grep -q '^ID=steamos$' /etc/os-release; then
+    steamos-readonly disable
+fi
 echo "downloading and installing me3..."
 curl --proto '=https' --tlsv1.2 -sSfL https://github.com/garyttierney/me3/releases/download/v0.11.0/installer.sh | sh
 echo "install done."
-cd ${HOME}
+cd "${1}" | exit 1
 git clone https://github.com/Daybreak-Team/MMV-Launcher
 echo "mod installed"
-cd "${HOME}/MMV-Launcher/"
+cd "${1}/MMV-Launcher/" | exit 1
 git clone https://github.com/EleriLove/More-Map-Variations-launch-script/
-cp "${HOME}/MMV-Launcher/More-Map-Variations-launch-script/launch_nightreign.sh" "${HOME}/MMV-Launcher/"
-rm -rf "${HOME}/MMV-Launcher/More-Map-Variations-launch-script"
-chmod +x "${HOME}/MMV-Launcher/launch_nightreign.sh"
+cp "${1}/MMV-Launcher/More-Map-Variations-launch-script/launch_nightreign.sh" "${1}/MMV-Launcher/"
+rm -rf "${1}/MMV-Launcher/More-Map-Variations-launch-script"
+chmod +x "${1}/MMV-Launcher/launch_nightreign.sh"
 echo "launcer installed, enjoy your game"
 sleep 20
+if grep -q '^ID=steamos$' /etc/os-release; then
+    steamos-readonly enable
+fi
