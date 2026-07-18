cb02a9b889a11693118cc8808129178c75f1366f
Eleri <141176534+EleriLove@users.noreply.github.com>
2026-07-18 13:48:52 -0400

    Update launch_nightreign.sh

--- launch_nightreign.sh
+++ launch_nightreign.sh
@@ -1,7 +1,7 @@
 mod_engine=$(/usr/bin/which me3)
 $mod_engine launch \
-  --native "${home}/MMV-Launcher/mod/Server Redirector/cl_server_redirector.dll" \
-  --package "${home}/MMV-Launcher/mod" \
+  --native "${HOME}/MMV-Launcher/mod/Server Redirector/cl_server_redirector.dll" \
+  --package "${HOME}/MMV-Launcher/mod" \
   -g nightreign \
   --savefile "NR0000.co2" \
   --online true
