2561b555432aa450dd637661e50582aae92b6941
SkriptKitty <elerilove@gmail.com>
2026-07-18 17:30:40 -0400

    this works, don't fuck with it.

--- launch_nightreign.sh
+++ launch_nightreign.sh
@@ -1,6 +1,6 @@
-server_redirector=$(find ~ -name "cl_server_redirector.dll")
-prog_path=$(find ~ -name "launch_nightreign.sh" 2>/dev/null)
-prog_dir=$(dirname "$prog_path")
+server_redirector=$(/usr/bin/find ~ -name "cl_server_redirector.dll" 2>/dev/null)
+prog_path=$(/usr/bin/find ~ -name "launch_nightreign.sh" 2>/dev/null)
+prog_dir=$(/usr/bin/dirname "$prog_path")
 mod_engine=$(/usr/bin/which me3)
 $mod_engine launch \
   --native "${server_redirector" \
