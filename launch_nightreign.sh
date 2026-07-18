5b7f40724eab3af049033f8ccbbd23b39c74268b
SkriptKitty <elerilove@gmail.com>
2026-07-18 17:26:59 -0400

    yes? works?

--- launch_nightreign.sh
+++ launch_nightreign.sh
@@ -1,7 +1,10 @@
+server_redirector=$(find ~ -name "cl_server_redirector.dll")
+prog_path=$(find ~ -name "launch_nightreign.sh" 2>/dev/null)
+prog_dir=$(dirname "$prog_path")
 mod_engine=$(/usr/bin/which me3)
 $mod_engine launch \
-  --native "${HOME}/MMV-Launcher/mod/Server Redirector/cl_server_redirector.dll" \
-  --package "${HOME}/MMV-Launcher/mod" \
+  --native "${server_redirector" \
+  --package "${prog_dir}mod" \
   -g nightreign \
   --savefile "NR0000.co2" \
   --online true
