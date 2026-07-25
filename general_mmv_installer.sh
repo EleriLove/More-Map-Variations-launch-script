08caf4d0c74e48e36c79714af384eb30de0eecd6
SkriptKitty <elerilove@gmail.com>
2026-07-24 20:55:58 -0400

    updated me3 install script to be up  to date with current me3 version

--- general_mmv_installer.sh
+++ general_mmv_installer.sh
@@ -3,7 +3,7 @@ if grep -q '^ID=steamos$' /etc/os-release; then
 fi
 prog_dir=$(find ~ -name "launch_nightreign.sh" 2>/dev/null)
 echo "downloading and installing me3..."
-curl --proto '=https' --tlsv1.2 -sSfL https://github.com/garyttierney/me3/releases/download/v0.11.0/installer.sh | sh
+curl --proto '=https' --tlsv1.2 -sSfL https://github.com/garyttierney/me3/releases/download/v0.12.1/installer.sh | sh
 echo "install done."
 cd "${1}" |exit 1
 git clone https://github.com/Daybreak-Team/MMV-Launcher "${1}"
