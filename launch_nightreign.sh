51c97e98e567c305c33be5b7bcb055d5c52545a7
SkriptKitty <elerilove@gmail.com>
2026-07-24 20:42:29 -0400

    fixed critical error in line 7 where I didn't have a / where I needed it,

--- launch_nightreign.sh
+++ launch_nightreign.sh
@@ -4,7 +4,7 @@ prog_dir=$(/usr/bin/dirname "$prog_path")
 mod_engine=$(/usr/bin/which me3)
 $mod_engine launch \
   --native "${server_redirector}" \
-  --package "${prog_dir}mod" \
+  --package "${prog_dir}/mod" \
   -g nightreign \
   --savefile "NR0000.co2" \
   --online true
