c8977b4e1e313cd1cdac2c5b2706c4bee4a8cf68
SkriptKitty <elerilove@gmail.com>
2026-07-29 18:54:01 -0400

    changed how the launch script so that it will get confused less often

--- launch_nightreign.sh
+++ launch_nightreign.sh
@@ -1,6 +1,6 @@
-server_redirector=$(/usr/bin/find ~ -name "cl_server_redirector.dll" 2>/dev/null)
 prog_path=$(/usr/bin/find ~ -name "launch_nightreign.sh" 2>/dev/null)
 prog_dir=$(/usr/bin/dirname "$prog_path")
+server_redirector=$(/usr/bin/find ${prog_dir} -name "cl_server_redirector.dll" 2>/dev/null)
 mod_engine=$(/usr/bin/which me3)
 $mod_engine launch \
   --native "${server_redirector}" \
