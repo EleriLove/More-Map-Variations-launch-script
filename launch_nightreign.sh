7b49b966fb22ac4e05e9c32151d10845fbcdbbdb
SkriptKitty <elerilove@gmail.com>
2026-08-24 23:31:39 -0400

    made launch script work differently for specificaly steamdeck in order to work better for game mode

--- launch_nightreign.sh
+++ launch_nightreign.sh
@@ -1,4 +1,5 @@
-prog_path=$(/usr/bin/find / -name "launch_nightreign.sh" 2>/dev/null)
+if !grep -q '^ID=steamos$' /etc/os-release; then
+    prog_path=$(/usr/bin/find / -name "launch_nightreign.sh" 2>/dev/null)
 prog_dir=$(/usr/bin/dirname "$prog_path")
 mod_engine=$(/usr/bin/which me3)
 $mod_engine launch \
@@ -9,3 +10,15 @@ $mod_engine launch \
   --savefile "NR0000.co2" \
   --online true \
   --disable_arxan true
+fi
+
+prog_dir="${1}"
+${HOME}/.local/bin/me3 launch \
+  --native "${prog_dir}/mod/Server Redirector/cl_server_redirector.dll" \
+  --native "${prog_dir}/mod/dll/custom_drop_fxrs.dll" \
+  --package "${prog_dir}/mod" \
+  -g nightreign \
+  --savefile "NR0000.co2" \
+  --online true \
+  --disable_arxan true
+
