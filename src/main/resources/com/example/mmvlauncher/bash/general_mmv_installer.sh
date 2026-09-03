3ad631bc0e99f1af07885e76bfee95dece34e6cc
JoshuaPendlebury <JPendlebury2375@gmail.com>
2026-09-03 16:30:00 +0000

    Built MMV-Launcher.jar on Linux Ubuntu and updated the Bash files

--- src/main/resources/com/example/mmvlauncher/bash/general_mmv_installer.sh
+++ src/main/resources/com/example/mmvlauncher/bash/general_mmv_installer.sh
@@ -1,23 +1,39 @@
-if grep -q '^ID=steamos$' /etc/os-release && ! sudo -n true 2>/dev/null; then
+if grep -q '^ID=steamos$' /etc/os-release  && sudo -n true 2>/dev/null; then
     echo "Sudo requires a password. Please set one with: passwd"
     exit 1
 fi
 if grep -q '^ID=steamos$' /etc/os-release; then
     sudo steamos-readonly disable
 fi
+if ! command -v git-lfs >/dev/null 2>&1; then
+    echo "Git LFS is required but is not installed."
+    if command -v apt-get >/dev/null 2>&1; then
+        sudo apt-get update
+        sudo apt-get install -y git-lfs
+	git lfs install
+    elif command -v pacman >/dev/null 2>&1; then
+        sudo pacman -Sy --noconfirm git-lfs
+	git lfs install
+    else
+        echo "Could not find a supported package manager to install Git LFS."
+	echo "please install it yourself"
+        exit 1
+    fi
+fi
 prog_dir=$(find ~ -name "launch_nightreign.sh" 2>/dev/null)
 echo "downloading and installing me3..."
 curl --proto '=https' --tlsv1.2 -sSfL https://github.com/garyttierney/me3/releases/download/v0.12.1/installer.sh | sh
 if ! echo $PATH |grep "${HOME}/.local/bin";  then
-    printf 'export PATH="$PATH:/home/deck/.local/bin"\n' >> ${HOME}/.bash_profile
+    export PATH="$PATH:/home/deck/.local/bin"
 fi
 echo "install done."
 mkdir -p "${1}" || {
    echo "mkdir failed"
    exit 1
 }
-cd "${1}" || exit 1
 git clone "https://github.com/Daybreak-Team/MMV-Launcher" "${1}"
+cd "${1}"
+git lfs pull
 echo "mod installed"
 cp "${prog_dir}" "${1}"
 echo "launcer installed, enjoy your game"
--- src/main/resources/com/example/mmvlauncher/bash/launch_nightreign.sh
+++ src/main/resources/com/example/mmvlauncher/bash/launch_nightreign.sh
@@ -1,11 +1,23 @@
-/etc/profile
-prog_path=$(/usr/bin/find / -name "launch_nightreign.sh" 2>/dev/null)
-prog_dir=$(/usr/bin/dirname "$prog_path")
-server_redirector=$(/usr/bin/find ${prog_dir} -name "cl_server_redirector.dll" 2>/dev/null)
-mod_engine=$(/usr/bin/which me3)
-$mod_engine launch \
-  --native "${server_redirector}" \
-  --package "${prog_dir}/mod" \
-  -g nightreign \
-  --savefile "NR0000.co2" \
-  --online true
+if !grep -q '^ID=steamos$' /etc/os-release; then
+    prog_path=$(/usr/bin/find / -name "launch_nightreign.sh" 2>/dev/null)
+    prog_dir=$(/usr/bin/dirname "$prog_path")
+    mod_engine=$(/usr/bin/which me3)
+    $mod_engine launch \
+      --native "${prog_dir}/mod/ServerRedirector/cl_server_redirector.dll" \
+      --native "${prog_dir}/mod/dll/custom_drop_fxrs.dll" \
+      --package "${prog_dir}/mod" \
+      -g nightreign \
+      --savefile "NR0000.co2" \
+      --online true \
+      --disable-arxan true
+else
+    prog_dir="${1}"
+    ${HOME}/.local/bin/me3 launch \
+      --native "${prog_dir}/mod/ServerRedirector/cl_server_redirector.dll" \
+      --native "${prog_dir}/mod/dll/custom_drop_fxrs.dll" \
+      --package "${prog_dir}/mod" \
+      -g nightreign \
+      --savefile "NR0000.co2" \
+      --online true \
+      --disable-arxan true
+fi
