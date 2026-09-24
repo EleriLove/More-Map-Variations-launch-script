850b35e623fa00ab6a5adb7f7b00b68c654b5208
SkriptKitty <elerilove@gmail.com>
2026-09-23 21:29:51 -0400

    file structure

--- general_mmv_installer.sh
+++ general_mmv_installer.sh
@@ -23,6 +23,8 @@ unzip "${1}/MMV.zip" -d "${1}"
 rm "${1}/"*.zip.*
 rm  *.zip
 mv * "MMV-Files"
+cp "${1}/MMV-Files/mod" "${1}"
+rm -rf "${1}/MMV-FIles/"
 echo "mod installed"
 cp "${prog_dir}" "${1}"
 echo "launcer installed, enjoy your game"
